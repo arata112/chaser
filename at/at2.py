3049291f86ddb9ee0d8d0c0d1be15e3619e0e530
arata112 <126308966+arata112@users.noreply.github.com>
2024-04-06 10:01:09 +0900

    Add files via upload

--- at/at2.py
+++ at/at2.py
@@ -22,7 +22,7 @@ def main():
     value = [] # フィールド情報を保存するリスト
     client = CHaser.Client() # サーバーと通信するためのインスタンス"
     muki = "left" # mukiをdownにする
-    turn = 0
+    turn = 100
 
     def look_walk_up():
         rand = random.randint(1,3) # 1から3の乱数を作る
@@ -30,9 +30,11 @@ def main():
             value = client.look_up() # 上の3×3マスを調べる
             if value[4] != 1: # もし2つ上が敵でなければ
                 value = client.get_ready()
+                turn = turn - 1  # get_readyをしたらturnを1減らす
                 value = client.walk_up()    
             else: # もし2つ上が敵なら
                 value = client.get_ready()
+                turn = turn - 1  # get_readyをしたらturnを1減らす
                 value = client.put_up()
         else: # 乱数が2か3なら
             value = client.walk_up() # 上に行く
@@ -43,9 +45,11 @@ def main():
             value = client.look_down() # 下の3×3マスを調べる
             if value[4] != 1: # もし2つ下が敵でなければ
                 value = client.get_ready()
+                turn = turn - 1  # get_readyをしたらturnを1減らす
                 value = client.walk_down()
             else: # もし2つ下が敵なら
                 value = client.get_ready()
+                turn = turn - 1  # get_readyをしたらturnを1減らす
                 value = client.put_down()
         else: # 乱数が2か3なら
             value = client.walk_down() # 下に行く 
@@ -78,8 +82,7 @@ def main():
                       
     while(True):
         value = client.get_ready() # 行動する前には必ず get_ready() する
-        print(muki)
-        print(turn)
+        turn = turn - 1  # get_readyをしたらturnを1減らす
 
         if value[1] == 1: # もし上が敵なら
             value = client.put_up() # 上にブロックをおく
@@ -158,77 +161,48 @@ def main():
                                        
         else: # 上も下も右もひだりもアイテムではないなら
 
-            rand = random.randint(0,1)
-            if rand == 0:
-                rand2 = random.randint(0,1)
-                if rand2 == 0:
+            rand = random.randint(0,4) # 0から4までの乱数を作る
+            if rand == 0: # randが0なら（5回に1回くらいサーチをする）
+                rand2 = random.randint(0,3) # rand2を作り
+                if rand2 == 0: # rand2が0なら
                     value = client.search_up() # 上９マスを調べる
-                    for i in range(9):
-                        if value[i] == 3:
-                            muki = "up"
+                    for i in range(9):  # 9回繰り返す(iは0から8まで1ずつ増える)
+                        if value[i] == 3: # もしi番目がアイテムなら
+                            muki = "up" # 向きをupに変える
                     value = client.get_ready()
+                elif rand2 == 1: # rand2が1なら
+                    value = client.search_down() # 下９マスを調べる
+                    for i in range(9):  # 9回繰り返す(iは0から8まで1ずつ増える)
+                        if value[i] == 3: # もしi番目がアイテムなら
+                            muki = "down" # 向きをdownに変える
+                    value = client.get_ready()   
+                elif rand2 == 2: # rand2が2なら
+                    value = client.search_right() # 下９マスを調べる
+                    for i in range(9):  # 9回繰り返す(iは0から8まで1ずつ増える)
+                        if value[i] == 3: # もしi番目がアイテムなら
+                            muki = "right" # 向きをrightに変える
+                    value = client.get_ready()
+                elif rand2 == 3: # rand2が3なら
+                    value = client.search_left() # 下９マスを調べる
+                    for i in range(9):  # 9回繰り返す(iは0から8まで1ずつ増える)
+                        if value[i] == 3: # もしi番目がアイテムなら
+                            muki = "left" # 向きをdownに変える
+                    value = client.get_ready()             
 
-
-            """
-            rand = random.randint(0,3)
-            if turn == 5: # もしturnが10なら
-                turn = 0
-                if rand == 0: #　もしrandが0なら上にいく
-                    if value[1] != 2:
-                        look_walk_up()
-                        muki = "up"
-                    elif value[5] != 2:
-                        look_walk_right()
-                        muki = "right"
-                    elif value[3] != 2:
-                        look_walk_left()
-                        muki = "left"
-                    elif value[7] != 2:
-                        look_walk_down()  
-                        muki ="down" 
-                elif rand == 1: #　もしrandが1なら右にいく
-                    if value[5] != 2:
-                        look_walk_right()
-                        muki = "right"
-                    elif value[1] != 2:
-                        look_walk_up()
-                        muki = "up"
-                    elif value[3] != 2:
-                        look_walk_left()    
-                        muki = "left"    
-                    elif value[7] != 2:
-                        look_walk_down()
-                        muki = "down"
-                elif rand == 2: # もしrandが１なら下に行く
-                    if value[7] != 2:
-                        look_walk_down()
-                        muki = "down"
-                    elif value[1] != 2:
-                        look_walk_up()
-                        muki = "up"
-                    elif value[5] != 2:
-                        look_walk_right()
-                        muki = "right"
-                    elif value[3] != 2:
-                        look_walk_left()
-                        muki = "left"           
-                elif rand == 3: # もしrandが3なら下に行く
-                    if value[3] != 2:
-                        look_walk_left()
-                        muki = "left"
-                    elif value[7] != 2:
-                        look_walk_down()
-                        muki = "down"
-                    elif value[5] != 2:
-                        look_walk_right()
-                        muki = "right"
-                    elif value[1] != 2:
-                        look_walk_up()
-                        muki = "up"
-            """
+            rand = random.randint(0,14) # 0から14までの乱数を作る
+            if rand == 0: # randが0なら（15回に1回くらいランダムに向きを変える）
+                rand = random.randint(0,3)
+                if rand == 0: #　もしrandが0ならmukiをupに変える
+                    muki = "up"
+                elif rand == 1: #　もしrandが0ならmukiをupに変える
+                    muki = "down"
+                elif rand == 2: 
+                    muki = "left"
+                elif rand == 3:
+                    muki = "right"          
+            
 
             if muki == "down": # もしmukiがdownなら
-                turn += 1 
                 if value[7] != 2: # もし自分の下がブロックではないなら またはアイテムなら
                     look_walk_down() # 下に行く   
                 elif value[3] != 2: # もし自分の下がブロックで左がブロックではないなら
@@ -242,7 +216,6 @@ def main():
                     muki = "up"  
 
             elif muki == "up": # もしmukiがupなら
-                turn += 1 
                 if value[1] != 2: # もし自分の上がブロックではないなら    
                     look_walk_up() # 上に行く     
                 elif value[5] != 2: # もし上がブロックで右がブロックでないなら     
@@ -256,7 +229,6 @@ def main():
                     muki = "down"
 
             elif muki == "right": # もしmukiがrightなら
-                turn += 1 
                 if value[5] != 2: # もし自分の右がブロックではないなら 
                     look_walk_right() # 右に行く 
                 elif value[7] != 2 : # 右がブロックで下がブロックでないなら
@@ -270,7 +242,6 @@ def main():
                     muki = "left"
 
             elif muki == "left": # もしmukiがleftなら
-                turn += 1 
                 if value[3] != 2: # もし自分の左がブロックではないなら 
                     look_walk_left() # 左に行く   
                 elif value[1] != 2: # 左がブロックで上がブロックでないなら
